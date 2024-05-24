1b982caf73d09a4310fd54ab9592d83b7b613349
Brandon Castillo <brajoel98@gmail.com>
2024-05-24 13:53:20 -0700

    feat: :safety_vest: add class-transformer to enable dto validations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(PORT);
+
+  console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
 }
 bootstrap();
