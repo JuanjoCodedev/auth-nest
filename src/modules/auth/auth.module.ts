d909b077060009fb87f15a3c98f3fb405b0b2edb
JuanjoCodedev <juansalazaraguirre0809@gmail.com>
2024-12-12 10:17:35 -0500

    se centralizan los modulos en auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,24 +1,62 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { UserEntity } from '../user/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { EMailerService } from '../mailer/mailer.service';
+
+/* Module */
+import { WithProviderModule } from './with-provider/with-provider.module';
+
+/* Entity */
+import { UserEntity } from '../user/user.entity';
+import { RolesEntity } from './roles/roles.entity';
+import { RolPermissionsEntity } from './rol_permissions/rol_permissions.entity';
+import { PermissionsEntity } from './permissions/permissions.entity';
+
+/* Strategy */
 import { GoogleStrategy } from 'src/strategy/google.strategy';
 import { GithubStrategy } from 'src/strategy/github.strategy';
-import { SignInModule } from './sign-in/sign-in.module';
-import { SignUpModule } from './sign-up/sign-up.module';
-import { TokensModule } from './tokens/tokens.module';
-import { WithProviderModule } from './with-provider/with-provider.module';
-import { ResetPasswordModule } from './reset-password/reset-password.module';
-import { RolesModule } from './roles/roles.module';
-import { PermissionsModule } from './permissions/permissions.module';
-import { RolPermissionsModule } from './rol_permissions/rol_permissions.module';
+
+/* Service */
+import { AuthService } from './auth.service';
+import { EMailerService } from '../mailer/mailer.service';
+import { SignInService } from './sign-in/sign-in.service';
+import { SignUpService } from './sign-up/sign-up.service';
+import { RolesService } from './roles/roles.service';
+import { RolPermissionsService } from './rol_permissions/rol_permissions.service';
+import { ResetPasswordService } from './reset-password/reset-password.service';
+import { PermissionsService } from './permissions/permissions.service';
+import { TokensService } from './tokens/tokens.service';
+
+/* Controller */
+import { SignInController } from './sign-in/sign-in.controller';
+import { SignUpController } from './sign-up/sign-up.controller';
+import { RolesController } from './roles/roles.controller';
+import { ResetPasswordController } from './reset-password/reset-password.controller';
+import { PermissionsController } from './permissions/permissions.controller';
+import { TokensController } from './tokens/tokens.controller';
+import { RolPermissionsController } from './rol_permissions/rol_permissions.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity]), PassportModule.register({ defaultStrategy: 'google' }), SignInModule, SignUpModule, TokensModule, WithProviderModule, ResetPasswordModule, RolesModule, PermissionsModule, RolPermissionsModule],
-  providers: [AuthService, JwtService, EMailerService, GoogleStrategy, GithubStrategy],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity, RolesEntity, RolPermissionsEntity, PermissionsEntity]),
+    PassportModule.register({ defaultStrategy: 'google' }),
+    WithProviderModule,
+  ],
+  providers: [
+    AuthService,
+    JwtService,
+    EMailerService,
+    GoogleStrategy,
+    GithubStrategy,
+    SignInService,
+    SignUpService,
+    RolesService,
+    RolPermissionsService,
+    ResetPasswordService,
+    PermissionsService,
+    TokensService,
+  ],
+  controllers: [SignInController, SignUpController, RolesController, RolPermissionsController, ResetPasswordController, PermissionsController, TokensController],
   exports: [JwtService, AuthService],
 })
 export class AuthModule {}
