61086420eca16c2c9a756cbc83a4a4ebe2a236ad
JuanjoCodedev <juansalazaraguirre0809@gmail.com>
2024-12-13 11:21:38 -0500

    feat: implementar servicio y filtro para registrar errores en la base de datos

---
+++ src/modules/log/log.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('log')
+export class LogEntity {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar' })
+  path: string;
+
+  @Column({ type: 'varchar' })
+  message: string;
+
+  @Column({ type: "int2" })
+  statusCode: number;
+
+  @Column({ type: 'varchar' })
+  client_ip: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date;
+}
--- src/shared/filter/http-exception.filter.ts
+++ src/shared/filter/http-exception.filter.ts
@@ -1,19 +1,42 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+/* Entity */
+import { LogDto } from 'src/modules/log/log.dto';
+
+/* Service */
+import { LogService } from 'src/modules/log/log.service';
+
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  constructor(private readonly _logService: LogService) {}
+
+  async catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+    const statusCode = exception.getStatus();
     const errorResponse = exception.getResponse();
+    const clientIp = request.headers['x-forwarded-for'] || request.socket.remoteAddress;
 
-    response.status(status).json({
-      statusCode: status,
+    const logData: LogDto = {
       path: request.url,
-      message: errorResponse,
+      message: exception.message,
+      statusCode: statusCode,
+      client_ip: String(clientIp),
+    };
+
+    try {
+      await this._logService.logCreate(logData);
+    } catch (logError) {
+      console.error('Error al registrar el log:', logError);
+    }
+
+    response.status(statusCode).json({
+      statusCode: statusCode,
+      path: request.url,
+      errorResponse: errorResponse,
+      clientIp: clientIp,
       timestamp: new Date().toISOString(),
     });
   }
